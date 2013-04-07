37319a179aa1d312b42c8c3a39e3cc268af2cbf9
aaron <hy.chen@hnair.com>
2013-04-07 15:12:59 +0800

    + ‘ten-minutes/.classpath’ + ‘ten-minutes/.project’ + ‘ten-minutes/.settings/org.eclipse.jdt.core.prefs’ + ‘ten-minutes/.settings/org.eclipse.wst.common.component’ + ‘ten-minutes/.settings/org.eclipse.wst.common.project.facet.core.xml’ + ‘ten-minutes/log.roo’ + ‘ten-minutes/pom.xml’ + ‘ten-minutes/src/main/java/com/tenminutes/Timer.java’ + ‘ten-minutes/src/main/java/com/tenminutes/Timer_Roo_Configurable.aj’ + ‘ten-minutes/src/main/java/com/tenminutes/Timer_Roo_JavaBean.aj’ ...

---
+++ ten-minutes/src/main/java/com/tenminutes/Timer.java
@@ -0,0 +1,15 @@
+package com.tenminutes;
+
+import javax.validation.constraints.NotNull;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord
+public class Timer {
+
+    @NotNull
+    private String message;
+}
